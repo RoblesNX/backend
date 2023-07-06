9e7c1bf380d937ca6dc76b6f874046db4c518309
Nicolas Robles <nicolasrobles@MacBook-Pro-de-Nicolas.local>
2023-07-05 22:39:55 -0300

    version final

--- Clase_001/index.js
+++ Clase_001/index.js
@@ -3,7 +3,7 @@ class ProductManager{
         this.products=[]
     
     }
-    static id=0
+    static id = 0
     addProduct(title, description, price, thumbmail, code, stock){
         ProductManager.id++
         this.products.push({title, description, price, thumbmail, code, stock, id:ProductManager.id});
@@ -21,15 +21,16 @@ class ProductManager{
         }
     }
 }
- const productos = new ProductManager();
 
+const productos = new ProductManager();
+
+console.log(productos.getProduct());
+
+ productos.addProduct('Nike SB Force 58 Premium','Calzado de moda',45999,'https://i.ibb.co/bQKdmwL/597344-1200-1200.webp','DV5476-001',8)
+ productos.addProduct('Nike Air Zoom Vomero 16','Calzado para running',84999,'https://i.ibb.co/Tc5mJnF/451724-1200-1200.webp','DA7245-401',22)
+ productos.addProduct('Nike Air Force 1 07 LV8','Calzado deportivo',74999,'https://i.ibb.co/7WXPj1M/531712-1200-1200.webp','DV0794-001',10)
+ 
  console.log(productos.getProduct());
 
- productos.addProduct('mesa','para comer',2000,'imagen.com','abc123',10)
- productos.addProduct('silla','para sentarse',1500,'imagen2.com','abc124',20)
-
- console.log(productos.getProduct());
-
+ console.log(productos.getProductById(4));
  console.log(productos.getProductById(2));
-
- console.log(productos.getProductById(3));
